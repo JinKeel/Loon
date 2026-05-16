0cca8d358c188a840971ffb324e5fe2a0c3480f9
Z <z-jinke@qq.com>
2026-05-16 17:54:49 +0800

    更新

--- script/kugou.js
+++ script/kugou.js
@@ -1,262 +1,282 @@
 let body = $response.body;
 let obj = JSON.parse(body);
 
-if (!obj?.data) {
-    $done({ body });
-}
-
-let d = obj.data;
-
-const setAllZero = o => {
-    for (let k in o) {
-        o[k] = "0";
+const setMap = (path, values) => {
+    if (path?.mapParams) {
+        Object.keys(values).forEach(key => {
+            if (key in path.mapParams) {
+                path.mapParams[key] = values[key];
+            }
+        });
     }
 };
 
-const setKeys = (o, keys, val = "0") => {
-    keys.forEach(k => {
-        o[k] = val;
-    });
+const clearArr = path => {
+    if (Array.isArray(path)) {
+        path.length = 0;
+    }
 };
 
-if (d.TMEAB0ad0type?.mapParams) {
-    setAllZero(d.TMEAB0ad0type.mapParams);
-}
+setMap(obj.data.TMEAB0tgviptype0type, {
+    songs_pop_ups_max: "0",
+    singlesong_pop_ups_max: "0",
+    Insert_ads_mode: "0",
+    Insert_ads_timing: "0",
+    Insert_ads_drag: "0",
+    Insert_ads_point: "0:0",
+    hide_tvbar: "1",
+    vip_free_mode: "1",
+    vip_free_times: "999999",
+    vip_free_timeperiod: "999999"
+});
 
-if (d.TMEAB0FreemiumExposureFix0type?.mapParams) {
-    setAllZero(d.TMEAB0FreemiumExposureFix0type.mapParams);
-}
+setMap(obj.data.freeproject, {
+    secondfloor: "0",
+    freecard: "0"
+});
 
-if (d.TMEAB0FreemiumExposureFix0typeV2?.mapParams) {
-    setAllZero(d.TMEAB0FreemiumExposureFix0typeV2.mapParams);
-}
+setMap(obj.data.landscapemode, {
+    playpage_landscape: "0"
+});
 
-if (d.TMEAB0vipplay0type?.mapParams) {
-    d.TMEAB0vipplay0type.mapParams.ad_seconds = "0";
-}
+setMap(obj.data.TMEAB0bofangballchudian0Type, {
+    is_show: "0",
+    bar_time: "0"
+});
 
-if (d.TMEAB0vipcenter0type?.mapParams) {
-    d.TMEAB0vipcenter0type.mapParams.vipcenter_exposure = "0";
-}
+setMap(obj.data.TMEAB0WelcomePage0type, {
+    show: "0"
+});
 
-if (d.TMEAB0yinzhivip0type?.mapParams) {
-    d.TMEAB0yinzhivip0type.mapParams.yinzhivip_up = "0";
-}
+setMap(obj.data.TMEAB0notpay0splashinterval, {
+    up_control: "0",
+    is_show_up: "0"
+});
 
-if (d.TMEAB0mianmo0type?.mapParams) {
-    setKeys(d.TMEAB0mianmo0type.mapParams, [
-        "vip_over",
-        "pay_up"
-    ]);
-}
+setMap(obj.data.TMEAB0barstyle0Type, {
+    is_show: "0"
+});
 
-if (d.viphide?.mapParams) {
-    setKeys(d.viphide.mapParams, [
-        "video_play_hide",
-        "song_mode_unvipsong",
-        "video_mode_hide",
-        "song_mode_hide"
-    ]);
-}
+setMap(obj.data.TMEAB0svipnewguoqiup0Type, {
+    up_style: "0",
+    after_time: "0",
+    long_after_time: "0",
+    before_time: "0",
+    youth_up_style: "0"
+});
 
-if (d.TMEAB0sharefree0type?.mapParams) {
-    setKeys(d.TMEAB0sharefree0type.mapParams, [
-        "share_free",
-        "player"
-    ]);
-}
+setMap(obj.data.TMEAB0vipshellwindow0type, {
+    before_overdue: "0",
+    before_overdue_control: "0",
+    after_overdue: "0",
+    overdue_control: "0",
+    after_overdue_control: "0"
+});
 
-if (d.TMEAB0Pulltorefresh0type?.mapParams) {
-    d.TMEAB0Pulltorefresh0type.mapParams.interaction = "0";
-}
+setMap(obj.data.TMEAB0signin0type, {
+    signin_daily: "0",
+    enter: "0",
+    back_up: "0"
+});
 
-if (d.collect?.mapParams) {
-    setKeys(d.collect.mapParams, [
-        "queue",
-        "songlist"
-    ]);
-}
+setMap(obj.data.TMEAB0FreemiumModelTouchpoint0type, {
+    progress_bar_ad_autoshut: "1",
+    hello_ad: "0",
+    radio_down_ad_con: "0",
+    progress_bar_ad: "0",
+    player_top_ad: "0",
+    radio_down_ad: "0",
+    progress_bar_ad_con: "0",
+    enter: "0",
+    radio_down_noremind: "1",
+    hello_ad_con: "0",
+    today_act_min: "0",
+    player_top_ad_con: "0"
+});
 
-if (d.TMEAB0favtoast?.mapParams) {
-    setKeys(d.TMEAB0favtoast.mapParams, [
-        "type",
-        "textType"
-    ]);
-}
+setMap(obj.data.TMEAB0fmBidding, {
+    allowRepeatRequest: "0",
+    cacheCount: "0",
+    rewardReqNewBeforeShow: "0",
+    rewardLocalCacheSize: "0",
+    rewardReqInterval: "999999",
+    rewardReqNewTimeout: "0",
+    disallowPreloadNext: "1",
+    expires_interval: "0",
+    rewardRealTimeReqInterval: "999999",
+    rewardAdLocalCache: "0"
+});
 
-if (d.playtiming?.mapParams) {
-    d.playtiming.mapParams.timing = "none";
-}
+setMap(obj.data.TMEAB0wvipguoqiup0Type, {
+    is_show: "0",
+    svip_expire_day: "0",
+    after_time: "0",
+    long_after_time: "0",
+    before_time: "0"
+});
 
-if (d.TMEAB0audition0type?.mapParams) {
-    setKeys(d.TMEAB0audition0type.mapParams, [
-        "way",
-        "secs"
-    ]);
-}
+setMap(obj.data.TMEAB0GNBad0splashexpcontrol, {
+    front_5s: "0"
+});
 
-if (d.ringtone?.mapParams) {
-    setAllZero(d.ringtone.mapParams);
-}
+setMap(obj.data.TMEAB0freesignin0type, {
+    enter: "0",
+    free_signin_daily: "0",
+    free_type: "0"
+});
 
-if (d.TMEAB0deviceup0type?.mapParams) {
-    setAllZero(d.TMEAB0deviceup0type.mapParams);
-}
+setMap(obj.data.TMEAB0RECsources0type, {
+    is_show: "0"
+});
 
-if (d.cube?.mapParams) {
-    setKeys(d.cube.mapParams, [
-        "home_mode",
-        "play_page_mode",
-        "dig_mode"
-    ]);
-}
+setMap(obj.data.newsongradio, {
+    is_show: "0"
+});
 
-if (d.changtingdaily?.mapParams) {
-    setKeys(d.changtingdaily.mapParams, [
-        "newuser",
-        "daily"
-    ]);
-}
+setMap(obj.data.TMEAB0TaskCenterEnter0type, {
+    TFC: "0",
+    style: "0",
+    ad: "0",
+    mypage: "0",
+    TaskDone: "0"
+});
 
-if (d.timecomment?.mapParams) {
-    d.timecomment.mapParams.timecomment = "0";
-}
+setMap(obj.data.TMEAB0EarningTab0type, {
+    Animation: "0",
+    Earning_Tab: "0"
+});
 
-if (d.TMEAB0bofangballchudian0Type?.mapParams) {
-    setKeys(d.TMEAB0bofangballchudian0Type.mapParams, [
-        "is_show",
-        "bar_time"
-    ]);
-}
+setMap(obj.data.cube, {
+    home_mode: "0",
+    play_page_mode: "0",
+    dig_mode: "0"
+});
 
-if (d.landscapemode?.mapParams) {
-    d.landscapemode.mapParams.playpage_landscape = "0";
-}
+setMap(obj.data.SearchListV2, {
+    topic: "0",
+    Search_tab: "0",
+    Collect: "0",
+    Channel_blocking: "0"
+});
 
-if (d.emptyqueue?.mapParams) {
-    d.emptyqueue.mapParams.position = "none";
-    d.emptyqueue.mapParams.recomend = "none";
-}
+setMap(obj.data.TMEAB0sharefree0type, {
+    player: "0",
+    not_wxplay: "0",
+    share_free: "0"
+});
 
-if (d.SearchListV2?.mapParams) {
-    setKeys(d.SearchListV2.mapParams, [
-        "topic",
-        "Search_tab",
-        "Collect",
-        "Channel_blocking"
-    ]);
-}
+setMap(obj.data.tzpost, {
+    newtzpost: "0"
+});
 
-if (d.TMEAB0notpay0splashinterval?.mapParams) {
-    d.TMEAB0notpay0splashinterval.mapParams.up_control = "0_0";
-    d.TMEAB0notpay0splashinterval.mapParams.is_show_up = "0";
-}
+setMap(obj.data.TMEAB0yinzhivip0type, {
+    yinzhivip_up: "0"
+});
 
-if (d.TMEAB0WelcomePage0type?.mapParams) {
-    d.TMEAB0WelcomePage0type.mapParams.show = "0";
-}
+setMap(obj.data.TMEAB0ad0type, {
+    full_play: "0",
+    notice: "0",
+    fullplay_request: "0",
+    syban: "0",
+    round_play: "0",
+    click: "0",
+    comment: "0",
+    syban_show: "0",
+    syban_type: "0",
+    diy_play: "0",
+    act: "0",
+    post: "0",
+    syban_sort: "0"
+});
 
-if (d.tzpost?.mapParams) {
-    d.tzpost.mapParams.newtzpost = "0";
-}
+setMap(obj.data.viphide, {
+    video_play_hide: "1",
+    song_mode_unvipsong: "1",
+    video_mode_hide: "1",
+    song_mode_hide: "1"
+});
 
-if (d.TMEAB0svipnewguoqiup0Type?.mapParams) {
-    let m = d.TMEAB0svipnewguoqiup0Type.mapParams;
+setMap(obj.data.ringtone, {
+    playlist_qp: "0",
+    player_qp: "0",
+    ringtone_sq: "0",
+    ringtone_qp: "0",
+    ringtone_more: "0"
+});
 
-    m.up_style = "0";
-    m.after_time = "0_0_0";
-    m.long_after_time = "0_0_0";
-    m.before_time = "0_0_0";
-    m.youth_up_style = "0";
-}
+setMap(obj.data.TMEAB0mianmo0type, {
+    pick_enter: "0",
+    vip_over: "0",
+    back_up: "0",
+    low_threshold: "0",
+    pay_up: "0"
+});
 
-if (d.TMEAB0vipshellwindow0type?.mapParams) {
-    let m = d.TMEAB0vipshellwindow0type.mapParams;
+setMap(obj.data.changtingdaily, {
+    newuser: "0",
+    daily: "0"
+});
 
-    m.before_overdue = "0";
-    m.before_overdue_control = "0_0_0_0_0";
-    m.after_overdue = "0";
-    m.overdue_control = "0_0_0_0_0";
-    m.after_overdue_control = "0_0_0_0_0";
-}
+setMap(obj.data.TMEAB0songupfirst0Type, {
+    play_track: "0",
+    seek: "0",
+    skip_track: "0"
+});
 
-if (d.TMEAB0signin0type?.mapParams) {
-    let m = d.TMEAB0signin0type.mapParams;
+setMap(obj.data.TMEAB0FreemiumExposureFix0typeV2, {
+    popup_display: "0",
+    hot_start: "0"
+});
 
-    m.signin_daily = "0_0_0_0_0";
-    m.enter = "0";
-    m.back_up = "0_0_0_0_0";
-}
+setMap(obj.data.TMEAB0Pulltorefresh0type, {
+    interaction: "0"
+});
 
-if (d.TMEAB0FreemiumModelTouchpoint0type?.mapParams) {
-    setAllZero(d.TMEAB0FreemiumModelTouchpoint0type.mapParams);
-}
+setMap(obj.data.TMEAB0deviceup0type, {
+    mjdevice_counttimes: "0",
+    mjdevice_countstop: "0",
+    mjdevice_noremind: "1",
+    mjdevice_stop: "0",
+    mjdevice_times: "0",
+    mjdevice_day: "0",
+    mjdevice_up: "0",
+    mjdevice_noremindday: "0"
+});
 
-if (d.aiwritesong?.mapParams) {
-    d.aiwritesong.mapParams.aiwritesong = "0";
-}
+setMap(obj.data.TMEAB0vipcenter0type, {
+    vipcenter_exposure: "0"
+});
 
-if (d.TMEAB0fmBidding?.mapParams) {
-    let m = d.TMEAB0fmBidding.mapParams;
+setMap(obj.data.TMEAB0FreemiumExposureFix0type, {
+    hot_start: "0",
+    api_resume_state: "0",
+    api_init: "0",
+    popup_display: "0",
+    token_fix: "0",
+    keep_front: "0"
+});
 
-    m.allowRepeatRequest = "0";
-    m.cacheCount = "0";
-    m.rewardReqNewBeforeShow = "0";
-    m.rewardLocalCache = "0";
-    m.rewardReqInterval = "0";
-    m.rewardReqNewTimeout = "0";
-    m.disallowPreloadNext = "1";
-    m.expires_interval = "0";
-    m.rewardRealTimeReqInterval = "0";
-    m.rewardAdLocalCache = "0";
-}
+setMap(obj.data.TMEAB0favtoast, {
+    type: "0",
+    textType: "0"
+});
 
-if (d.TMEAB0wvipguoqiup0Type?.mapParams) {
-    let m = d.TMEAB0wvipguoqiup0Type.mapParams;
+setMap(obj.data.TMEAB0vipplay0type, {
+    vipplay_type: "0",
+    ad_seconds: "0"
+});
 
-    m.is_show = "0";
-    m.svip_expire_day = "0";
-    m.after_time = "0_0_0";
-    m.long_after_time = "0_0_0";
-    m.before_time = "0_0_0";
-}
-
-if (d.TMEAB0GNBad0splashexpcontrol?.mapParams) {
-    d.TMEAB0GNBad0splashexpcontrol.mapParams.front_5s = "0";
-}
-
-if (d.TMEAB0freesignin0type?.mapParams) {
-    let m = d.TMEAB0freesignin0type.mapParams;
-
-    m.enter = "0";
-    m.free_signin_daily = "0_0_0_0_0";
-    m.free_type = "0";
-}
-
-if (d.TMEAB0RECsources0type?.mapParams) {
-    d.TMEAB0RECsources0type.mapParams.is_show = "0";
-}
-
-if (d.TMEAB0TaskCenterEnter0type?.mapParams) {
-    let m = d.TMEAB0TaskCenterEnter0type.mapParams;
-
-    m.TFC = "0_0";
-    m.style = "0";
-    m.ad = "0";
-    m.mypage = "0_0_0_0_0";
-    m.TaskDone = "0_0_0_0_0";
-}
-
-if (d.TMEAB0EarningTab0type?.mapParams) {
-    let m = d.TMEAB0EarningTab0type.mapParams;
-
-    m.Animation = "0_0_0_0";
-    m.Earning_Tab = "0";
-}
-
-if (d.TMEAB0OfflineH5) {
-    d.TMEAB0OfflineH5 = [];
-}
+clearArr(obj.data.popupVIP);
+clearArr(obj.data.promoteplay);
+clearArr(obj.data.importlist);
+clearArr(obj.data.popup);
+clearArr(obj.data.Collects);
+clearArr(obj.data.TMEAB0OfflineH5);
+clearArr(obj.data.MusicHall);
+clearArr(obj.data.SelfpageUI);
+clearArr(obj.data.ExplorePageV5);
+clearArr(obj.data.LongAudio);
+clearArr(obj.data.TMEAB0landscapemode0type);
 
 body = JSON.stringify(obj);
 
