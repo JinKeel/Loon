4268ad5d6e058d1ce75f7306227b10eeca258abf
Z <z-jinke@qq.com>
2026-06-24 00:20:03 +0800

    Refactor variable declarations and fix naming conventions

--- JS/iP-Api.js
+++ JS/iP-Api.js
@@ -1,9 +1,9 @@
-const inputParams = $environment.params;
-const nodeName = inputParams.nodeInfo.name;
+var inputParams = $environment.params;
+var nodeName = inputParams.nodeInfo.name;
 
 $httpClient.get(
     { url: "http://ip-api.com/json/?lang=zh-CN", timeout: 5000, node: nodeName },
-  function (err, res, data) {
+    function (err, res, data) {
         if (err) {
             $done({
                 title: "查询超时",
@@ -12,9 +12,9 @@ $httpClient.get(
             return;
         }
 
-        const ip-api = JSON.parse(data);
+        var ipApi = JSON.parse(data);
 
-        if (ip-api.status !== "success") {
+        if (ipApi.status !== "success") {
             $done({
                 title: "查询失败",
                 message: "接口返回异常或返回无内容"
@@ -22,26 +22,26 @@ $httpClient.get(
             return;
         }
 
-        const html = `
+        var html = `
 <p style="text-align:center; font-family:-apple-system; line-height:1.8;">
     <br>
 
     <span style="font-size:17.2px; font-weight:600;color:#1599FF;">
-        IP ${ip-api.query}
+        IP ${ipApi.query}
     </span>
 
     <span style="color:#AAA;">─────────────────────</span><br>
 
     <span style="font-size:15px; font-weight:500;">
-        位置：${ip-api.country} ${ip-api.countryCode}
+        位置：${ipApi.country} ${ipApi.countryCode}
     </span><br>
 
     <span style="font-size:15px; font-weight:500;">
-        城市：${ip-api.regionName}
+        城市：${ipApi.regionName}
     </span><br>
 
     <span style="font-size:15px; font-weight:500;">
-        运营：${ip-api.isp}
+        运营：${ipApi.isp}
     </span><br>
 
     <span style="color:#AAA;">─────────────────────</span><br>
